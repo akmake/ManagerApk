a795367023fda85ffc7b5e04310b31698f6bc2a5
yosefdahan <92978132+akmake@users.noreply.github.com>
2026-04-21 16:59:00 +0300

    2

--- app/src/main/java/com/sterni/dailystudy/DailyStudyApp.kt
+++ app/src/main/java/com/sterni/dailystudy/DailyStudyApp.kt
@@ -1,13 +1,7 @@
 package com.sterni.dailystudy
 
 import android.app.Application
-import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DailyStudyApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        PDFBoxResourceLoader.init(applicationContext)
-    }
-}
+class DailyStudyApp : Application()
